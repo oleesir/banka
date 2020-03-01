a0af52e5b674f3c474aead3ec2e62eb2cfec9469
oleesir <oleesir@users.noreply.github.com>
2020-03-01 12:18:12 +0100

    implement ft-loggedin-user-171557432 (#36)

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
+import cors from 'cors';
 import express from 'express';
+
 import routes from './routes';
 import ErrorHandler from './middlewares/errorHandler';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -17,7 +22,7 @@ app.get('*', (req, res) => res.status(404).send({
   error: 'Not found'
 }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port);
 
